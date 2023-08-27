78d65039a64f5d705a4e4af7b95f3fc1ec1649d6
gribera <riberapelaez@gmail.com>
2023-08-27 20:40:29 +0200

    Model Category And Data Anotation

---
+++ Bulky/BulkyWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        /* Data Anotation permite que declaremos las restricciones de la tabla
+         * Ejmp : [KEY] Quien es la Primary Key o si es el nombre de la clase + Id CategoryId lo asignará automáticamente 
+          Más info en  https://learn.microsoft.com/en-us/aspnet/mvc/overview/older-versions/mvc-music-store/mvc-music-store-part-6     
+         */
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
